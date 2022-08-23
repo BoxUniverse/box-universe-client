d409f5319409f596e711b9ab7efce2e7be19af6d
kidp2h <thjnhsoajca@gmail.com>
2022-08-23 11:33:04 +0700

    setup page nextjs with layout

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,100 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import { CookieSerializeOptions } from 'cookie';
+import CredentialsProvider from '@providers/CredentialsProvider';
+import FacebookProvider from '@providers/FacebookProvider';
+import GoogleProvider from '@providers/GoogleProvider';
+import GithubProvider from '@providers/GithubProvider';
+import * as crypto from 'crypto';
+
+import client from '@src/ApolloClient';
+import OAuth from '@mutations/OAuth.graphql';
+import login from '@mutations/login.graphql';
+
+import { random } from 'lodash';
+
+export const cookiesOptions: CookieSerializeOptions = {
+  sameSite: 'lax',
+  secure: true,
+  httpOnly: true,
+  path: '/',
+};
+
+export const authOptions: NextAuthOptions = {
+  providers: [CredentialsProvider, FacebookProvider, GoogleProvider, GithubProvider],
+  session: {
+    strategy: 'jwt',
+    maxAge: 60 * 60 * 24 * 14,
+  },
+  jwt: {
+    maxAge: 60 * 60 * 24 * 14,
+    secret: process.env.SECRET,
+  },
+  pages: {
+    signIn: '/auth/login',
+    signOut: '/logout',
+    error: '/auth/login',
+    newUser: '/auth/register',
+    verifyRequest: '/auth/verify',
+  },
+  cookies: {
+    sessionToken: {
+      name: 'accessToken',
+      options: cookiesOptions,
+    },
+    csrfToken: {
+      name: 'csrfToken',
+      options: cookiesOptions,
+    },
+    callbackUrl: {
+      name: 'callbackUrl',
+      options: cookiesOptions,
+    },
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account.provider) {
+        const username = user.email?.split('@')[0];
+        const data = `${user.id}.${account.provider}.${process.env.SECRET}`;
+        const hash = crypto.createHash('md5').update(data).digest('hex');
+        try {
+          await client.mutate({
+            mutation: OAuth,
+            variables: {
+              OAuthInput: {
+                username,
+                email: user.email,
+                nonce: hash,
+                provider: account.provider,
+                id: user.id,
+              },
+            },
+          });
+        } catch (error: any) {
+          console.log(error);
+
+          return false;
+        }
+      }
+
+      return !!user;
+    },
+    async session({ session, token }) {
+      session.username = token.username;
+      session.email = token.email;
+      session._id = token._id;
+      session.user!.name = token.username as string;
+      session.user!.email = token.email as string;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user?.username) {
+        token.username = user.username;
+        token.email = user.email;
+        token._id = user._id;
+      }
+
+      return token;
+    },
+  },
+};
+export default NextAuth(authOptions);
---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,264 @@
+import React, { ReactElement, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import {
+  IoArrowForwardOutline,
+  IoEyeOffOutline,
+  IoEyeOutline,
+  IoKeyOutline,
+  IoLogoFacebook,
+  IoLogoGithub,
+  IoLogoGoogle,
+  IoPersonOutline,
+  IoWarningOutline,
+} from 'react-icons/io5';
+import { Checkbox, FormControlLabel, FormGroup, Typography } from '@mui/material';
+import { yupResolver } from '@hookform/resolvers/yup';
+import LoginSchema from '@validations/LoginSchema';
+import { NextPageWithLayout } from '../_app';
+import AuthLayout from '@layouts/AuthLayout';
+import Head from 'next/head';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import useThrottle from '@hooks/useThrottle';
+import { signIn } from 'next-auth/react';
+
+type Inputs = { username: string; password: string };
+const Login: NextPageWithLayout = () => {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    mode: 'onChange',
+    resolver: yupResolver(LoginSchema),
+  });
+
+  const [disable, setDisable] = useState<boolean>(false);
+
+  const login = async (username: string, password: string) => {
+    try {
+      const response = await signIn('credentials', { username, password, redirect: true });
+      const error = response?.error;
+
+      if (error) return Promise.reject(new Error('Unauthorizied'));
+    } catch (error) {
+      return Promise.reject(new Error('Unauthorized'));
+    }
+  };
+
+  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
+    if (!disable) {
+      setDisable(true);
+      const { username, password } = data;
+
+      toast.promise(
+        login(username, password),
+        {
+          success: {
+            render() {
+              return `Sign in to universe success !! You will automatically redirect to index`;
+            },
+            progressClassName: 'bg-purple-500',
+            onClose: () => {
+              router.push('/', undefined, { shallow: true });
+            },
+          },
+          error: {
+            render() {
+              setDisable(false);
+              return 'Oops! Authorize failed, please try again or check your username and password';
+            },
+          },
+        },
+        {
+          position: 'top-right',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark',
+          icon: <IoWarningOutline size={20} className="text-purple-500" />,
+          bodyClassName: 'bg-transparent',
+          className: 'bg-transparent backdrop-blur-sm border-purple-500',
+          pauseOnFocusLoss: false,
+        },
+      );
+    }
+  };
+  const debounceOnSubmit = useThrottle(onSubmit, 1000);
+  const [isHidden, setHidden] = useState<boolean>(true);
+  const handleInspectPassword = () => {
+    setHidden(!isHidden);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <form
+        onSubmit={handleSubmit(debounceOnSubmit)}
+        className="flex flex-col form-auth justify-center items-center h-full w-8/12 lg:w-full">
+        <div>
+          <FormGroup className="mb-5">
+            <label
+              htmlFor="username"
+              className="text-white select-none text-xl uppercase flex flex-row items-center mb-5 ">
+              Username
+            </label>
+
+            <div className="relative w-fit">
+              {errors.username && (
+                <span className="text-red-500 mb-0 text-sm absolute -top-6 left-0 bg-transparent">
+                  {errors.username.message}
+                </span>
+              )}
+              <input
+                {...register('username')}
+                name="username"
+                placeholder="username"
+                id="username"
+                className={`placeholder:select-none neon ${
+                  errors.username && 'error'
+                } text-lg align-middle placeholder:text-sm placeholder:uppercase w-60 h-12 bg-transparent rounded-lg focus: outline-0  pl-10 pr-12 border-purple-500`}
+                autoComplete="off"
+                spellCheck="false"
+              />
+              <IoPersonOutline
+                color="white"
+                className={`absolute top-1/2 transform -translate-y-1/2 ml-3 ${
+                  errors.username ? 'text-red-500' : 'text-purple-500'
+                }`}
+                size={20}
+              />
+            </div>
+          </FormGroup>
+          <FormGroup>
+            <label
+              htmlFor="password"
+              className="text-white select-none text-xl uppercase flex flex-row items-center mb-5">
+              Password
+            </label>
+            <div className="relative w-fit">
+              {errors.password && (
+                <span className="text-red-500 mb-0 text-sm absolute -top-6 left-0 bg-transparent">
+                  {errors.password.message}
+                </span>
+              )}
+              {isHidden ? (
+                <IoEyeOutline
+                  color="white"
+                  size={20}
+                  className={`cursor-pointer ${
+                    errors.password ? 'text-red-500' : 'text-purple-500'
+                  } absolute top-1/2 right-5 transform -translate-y-1/2`}
+                  onClick={handleInspectPassword}
+                />
+              ) : (
+                <IoEyeOffOutline
+                  color="white"
+                  size={20}
+                  className={`cursor-pointer ${
+                    errors.password ? 'text-red-500' : 'text-purple-500'
+                  } absolute top-1/2 right-5 transform -translate-y-1/2`}
+                  onClick={handleInspectPassword}
+                />
+              )}
+              <input
+                {...register('password')}
+                name="password"
+                placeholder="Password"
+                type={isHidden ? 'password' : 'text'}
+                id="password"
+                className={`placeholder:select-none neon ${
+                  errors.password && 'error'
+                } text-lg align-middle placeholder:text-sm placeholder:uppercase w-60 h-12 bg-transparent rounded-lg focus: outline-0  pl-10 pr-12 border-purple-500`}
+                autoComplete="off"
+                spellCheck="false"
+              />
+              <IoKeyOutline
+                color="white"
+                className={`absolute top-1/2 transform -translate-y-1/2 ml-3 ${
+                  errors.password ? 'text-red-500' : 'text-purple-500'
+                }`}
+                size={20}
+              />
+            </div>
+          </FormGroup>
+          <FormGroup className="flex flex-row justify-self-start self-start mt-5">
+            <FormControlLabel
+              control={<Checkbox defaultChecked className="text-purple-500" />}
+              label={
+                <Typography variant="inherit" color="white">
+                  Remember password
+                </Typography>
+              }
+              className="text-white text-xs"
+            />
+          </FormGroup>
+          <FormGroup className="mt-5 h-fit justify-self-start self-start ">
+            <div className="buttonIcon relative hover:bg-purple-500 rounded-lg w-fit ">
+              <input
+                type="submit"
+                className="w-16 h-16 z-50 border-purple-500 rounded-lg btnNeon text-transparent hover:bg-purple-500 cursor-pointer "
+              />
+              <IoArrowForwardOutline
+                color="white"
+                size={20}
+                className="absolute top-1/2 z-30 left-1/2 transform -translate-y-1/2 -translate-x-1/2 cursor-pointer"
+              />
+            </div>
+          </FormGroup>
+
+          <FormGroup className="flex text-white justify-center flex-col items-center mb-5 mt-2">
+            <div className="flex flex-row items-center mb-2">
+              <div className="h-1 w-16 border-y border-purple-500 mr-3 border-t-transparent"></div>
+              <span className="uppercase">login with</span>
+              <div className="h-1 w-16 border-y ml-3 border-purple-500 border-t-transparent"></div>
+            </div>
+            <div className="flex-row flex justify-around w-full mb-3">
+              <IoLogoGoogle
+                size={30}
+                className="text-red-500 cursor-pointer"
+                onClick={() => {
+                  signIn('google');
+                }}
+              />
+              <IoLogoFacebook
+                size={30}
+                onClick={() => {
+                  signIn('facebook');
+                }}
+                className="text-cyan-500 cursor-pointer"
+              />
+              <IoLogoGithub
+                className="text-black cursor-pointer"
+                size={30}
+                onClick={() => {
+                  signIn('github');
+                }}
+              />
+            </div>
+          </FormGroup>
+
+          <FormGroup className="flex flex-row text-xs mt-3 self-start text-red-500 uppercase">
+            <span className="mr-2 text-white">Don&apos;t have account ?</span>
+            <Link href="/auth/register">Register now</Link>
+          </FormGroup>
+        </div>
+      </form>
+    </>
+  );
+};
+
+Login.getLayout = function getLayout(page: ReactElement) {
+  return <AuthLayout>{page}</AuthLayout>;
+};
+
+export default Login;
